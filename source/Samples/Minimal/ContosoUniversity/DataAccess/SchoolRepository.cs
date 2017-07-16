89f31e8868798411aad8695a70ddc97a1ba3bafa
Pedro Silva <DarthPedro@gmail.com>
2017-07-15 23:46:12 -0700

    Setup the model and data repository for this sample. Create pages to manipulate students. Create a couple of ViewModels to start.

---
+++ source/Samples/Minimal/ContosoUniversity/DataAccess/SchoolRepository.cs
@@ -0,0 +1,27 @@
+﻿using ContosoUniversity.Models;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.DataAccess
+{
+    public class SchoolRepository
+    {
+        public SchoolRepository()
+        {
+            DataInitializer.Initialize(this);
+        }
+
+        public IList<Student> Students { get; set; } = new List<Student>();
+
+        public IList<Instructor> Instructors { get; set; } = new List<Instructor>();
+
+        public IList<Department> Departments { get; set; } = new List<Department>();
+
+        public IList<Course> Courses { get; set; } = new List<Course>();
+
+        public IList<OfficeAssignment> OfficeAssignments { get; set; } = new List<OfficeAssignment>();
+
+        public IList<CourseAssignment> CourseAssignments { get; set; } = new List<CourseAssignment>();
+
+        public IList<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+    }
+}
---
+++ source/Samples/Minimal/ContosoUniversity/Helpers/FrameHelper.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ContosoUniversity.Helpers
+{
+    public static class FrameHelper
+    {
+        public static Frame MainFrame
+        {
+            get { return (Frame)Window.Current.Content; }
+        }
+    }
+}
